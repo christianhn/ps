34d0cac76f1c5b978e1ce7879affcdc4bdf37dd5
Christian Hermoso <crishermosoj@gmail.com>
2026-03-27 11:50:42 +0100

    feat: Add conditional GitHub Pages deployment for feature branches and make Astro base URL configurable.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
 
+const BASE_URL = process.env.BASE_URL || '/ps';
+
 export default defineConfig({
   site: 'https://christianhn.github.io', 
-  base: '/ps',
+  base: BASE_URL,
   trailingSlash: 'always', 
   integrations: [
     sitemap(),
